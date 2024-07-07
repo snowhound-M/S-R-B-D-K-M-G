26bc2ab78b895e0468fd2c4194cb46464433d00a
snowhound-M <atulpundir147@gmail.com>
2024-07-08 01:15:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int("28879953")
+API_HASH = "358fb7e82480e714304f8e87a0ae2b1b"
+BOT_TOKEN = "6621306807:AAEk2tqhwdH3H3zGkyXm2QJPeJUKeBsnmrY"
+OWNER_ID = list(map(int, "1303122772"))
+MONGO_DB = "mongodb+srv://snowhound_03:N7W84R7rBVhhRowA@cluster0.nryirld.mongodb.net/?authMechanism=SCRAM-SHA-1"
+LOG_GROUP = int("-1002062268675")
+CHANNEL_ID = int("-1001846795972")
