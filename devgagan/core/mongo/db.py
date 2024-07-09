0081bab4de33c67b81fd380c0abd93dbefa0a214
snowhound-M <atulpundir147@gmail.com>
2024-07-09 22:39:37 +0530

    Update db.py

--- devgagan/core/mongo/db.py
+++ devgagan/core/mongo/db.py
@@ -3,95 +3,54 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 mongo = MongoCli(MONGO_DB)
 db = mongo.user_data
-db = db.users_data_db
-
-
+dc = db.users_data_db
 
 
 async def get_data(user_id):
-    x = await db.find_one({"_id": user_id})
+    x = await dc.find_one({"user_id": user_id})
     return x
 
 
 async def set_thumbnail(user_id, thumb):
     data = await get_data(user_id)
-    if data and data.get("_id"):
-        await db.update_one({"_id": user_id}, {"$set": {"thumb": thumb}})
+    if data and data.get("user_id"):
+        await dc.update_one({"user_id": user_id}, {"$set": {"thumb": thumb}})
     else:
-        await db.insert_one({"_id": user_id, "thumb": thumb})
+        await dc.insert_one({"user_id": user_id, "thumb": thumb})
 
 
 async def set_caption(user_id, caption):
     data = await get_data(user_id)
-    if data and data.get("_id"):
-        await db.update_one({"_id": user_id}, {"$set": {"caption": caption}})
+    if data and data.get("user_id"):
+        await dc.update_one({"user_id": user_id}, {"$set": {"caption": caption}})
     else:
-        await db.insert_one({"_id": user_id, "caption": caption})
-
-
-async def replace_caption(user_id, replace_txt, to_replace):
-    data = await get_data(user_id)
-    if data and data.get("_id"):
-        await db.update_one({"_id": user_id}, {"$set": {"replace_txt": replace_txt, "to_replace": to_replace}})
-    else:
-        await db.insert_one({"_id": user_id, "replace_txt": replace_txt, "to_replace": to_replace})
+        await dc.insert_one({"user_id": user_id, "caption": caption})
 
 
 async def set_session(user_id, session):
     data = await get_data(user_id)
-    if data and data.get("_id"):
-        await db.update_one({"_id": user_id}, {"$set": {"session": session}})
+    if data and data.get("user_id"):
+        await dc.update_one({"user_id": user_id}, {"$set": {"session": session}})
     else:
-        await db.insert_one({"_id": user_id, "session": session})
-
-
-
-async def clean_words(user_id, new_clean_words):
-    data = await get_data(user_id)
-    if data and data.get("_id"):
-        existing_words = data.get("clean_words", [])
-        # Ensure existing_words is a list
-        if existing_words is None:
-            existing_words = []
-        updated_words = list(set(existing_words + new_clean_words))
-        await db.update_one({"_id": user_id}, {"$set": {"clean_words": updated_words}})
-    else:
-        await db.insert_one({"_id": user_id, "clean_words": new_clean_words})
-
-
-async def remove_clean_words(user_id, words_to_remove):
-    data = await get_data(user_id)
-    if data and data.get("_id"):
-        existing_words = data.get("clean_words", [])
-        updated_words = [word for word in existing_words if word not in words_to_remove]
-        await db.update_one({"_id": user_id}, {"$set": {"clean_words": updated_words}})
-    else:
-        await db.insert_one({"_id": user_id, "clean_words": []})
+        await dc.insert_one({"user_id": user_id, "session": session})
 
 
 async def set_channel(user_id, chat_id):
     data = await get_data(user_id)
-    if data and data.get("_id"):
-        await db.update_one({"_id": user_id}, {"$set": {"chat_id": chat_id}})
+    if data and data.get("user_id"):
+        await dc.update_one({"user_id": user_id}, {"$set": {"chat_id": chat_id}})
     else:
-        await db.insert_one({"_id": user_id, "chat_id": chat_id})
+        await dc.insert_one({"user_id": user_id, "chat_id": chat_id})
 
-
-
-async def all_words_remove(user_id):
-    await db.update_one({"_id": user_id}, {"$set": {"clean_words": None}})
 
 async def remove_thumbnail(user_id):
-    await db.update_one({"_id": user_id}, {"$set": {"thumb": None}})
+    await dc.update_one({"user_id": user_id}, {"$set": {"thumb": None}})
 
 async def remove_caption(user_id):
-    await db.update_one({"_id": user_id}, {"$set": {"caption": None}})
-
-async def remove_replace(user_id):
-    await db.update_one({"_id": user_id}, {"$set": {"replace_txt": None, "to_replace": None}})
+    await dc.update_one({"user_id": user_id}, {"$set": {"caption": None}})
     
 async def remove_session(user_id):
-    await db.update_one({"_id": user_id}, {"$set": {"session": None}})
+    await dc.update_one({"user_id": user_id}, {"$set": {"session": None}})
 
 async def remove_channel(user_id):
-    await db.update_one({"_id": user_id}, {"$set": {"chat_id": None}})
+    await dc.update_one({"user_id": user_id}, {"$set": {"chat_id": None}})
