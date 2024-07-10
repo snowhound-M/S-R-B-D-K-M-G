f5e82960a3f5b56deec25175c397a99b0531dcde
snowhound-M <atulpundir147@gmail.com>
2024-07-10 11:20:00 +0530

    Update stats.py

--- devgagan/modules/stats.py
+++ devgagan/modules/stats.py
@@ -5,19 +5,6 @@ from devgagan.core.mongo.users_db import get_users, add_user, get_user
 from devgagan.core.mongo.plans_db import premium_users
 
 
-
-
-@app.on_message(group=10)
-async def chat_watcher_func(_, message):
-    try:
-        if message.from_user:
-            us_in_db = await get_user(message.from_user.id)
-            if not us_in_db:
-                await add_user(message.from_user.id)
-    except:
-        pass
-
-
 @app.on_message(filters.command("stats"))
 async def stats(client, message):
     users = len(await get_users())
